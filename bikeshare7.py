61226d145766d0a2a45de5f880d7394ef4a2fa5f
derSchmie <therealschmie@hotmail.com>
2026-04-09 13:03:48 +0200

    bikeshare7.py - Adjusted restart message

--- bikeshare7.py
+++ bikeshare7.py
@@ -287,7 +287,7 @@ def main():
         user_stats(df)
         raw_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart and adjust the filters? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
